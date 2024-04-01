a1b9af9b86be61af37cc82e6475a839b7299017e
Trysten M <nick.tristen22@gmail.com>
2024-04-02 02:03:03 +0300

    update(config/vite): update vite config
    add postcss config, global scss variables & path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+import autoprefixer from 'autoprefixer'
+import * as path from "node:path";
+
+export default defineConfig(({mode}) => {
+    const base = mode === 'production' ? '/task-manager/' : '/'
+    return {
+        plugins: [vue()],
+        css: {
+            postcss: {
+                plugins: [autoprefixer]
+            },
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "src/assets/css/variables/index.scss";`,
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        base
+    }
 })
